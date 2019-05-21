22fdac77ff7b57c0113fe4db9d2af657f77ede97
Brian Crafton <crafton.b@husky.neu.edu>
2019-05-21 09:24:31 -0400

    loading weights, changing batch norm to do fc layers

--- flappy_bird_bn.py
+++ flappy_bird_bn.py
@@ -9,17 +9,19 @@ import random
 import matplotlib.pyplot as plt
 
 from lib.Model import Model
+
 from lib.Layer import Layer 
 from lib.ConvToFullyConnected import ConvToFullyConnected
 from lib.FullyConnected import FullyConnected
 from lib.Convolution import Convolution
 from lib.MaxPool import MaxPool
 from lib.BatchNorm import BatchNorm
+
 from lib.Activation import Activation
 from lib.Activation import Relu
 from lib.Activation import Linear
 
-batch_size = 32
+batch_size = 64
 epsilon = 0.05
 decay = 0.99
 total_steps = int(1e6)
@@ -76,28 +78,32 @@ s = tf.placeholder("float", [None, 80, 80, 4])
 a = tf.placeholder("float", [None, 2])
 y = tf.placeholder("float", [None])
 
-l1_1 = Convolution(input_sizes=[batch_size, 80, 80, 4], filter_sizes=[8, 8, 4, 32], init='alexnet', strides=[1,4,4,1], padding="SAME", name='conv1')
-l1_2 = BatchNorm(input_size=[batch_size, 20, 20, 32], name='conv1_bn')
+l1_1 = Convolution(input_sizes=[batch_size, 80, 80, 4], filter_sizes=[8, 8, 4, 32], init='alexnet', strides=[1,4,4,1], padding="SAME", name='conv1', load=weights_conv)
+l1_2 = BatchNorm(input_size=[batch_size, 20, 20, 32], name='conv1_bn', load=weights_conv)
 l1_3 = Relu()
 l1_4 = MaxPool(size=[batch_size, 20, 20, 32], ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-l2_1 = Convolution(input_sizes=[batch_size, 10, 10, 32], filter_sizes=[4, 4, 32, 64], init='alexnet', strides=[1,2,2,1], padding="SAME", name='conv2')
-l2_2 = BatchNorm(input_size=[batch_size, 5, 5, 64], name='conv2_bn')
+l2_1 = Convolution(input_sizes=[batch_size, 10, 10, 32], filter_sizes=[4, 4, 32, 64], init='alexnet', strides=[1,2,2,1], padding="SAME", name='conv2', load=weights_conv)
+l2_2 = BatchNorm(input_size=[batch_size, 5, 5, 64], name='conv2_bn', load=weights_conv)
 l2_3 = Relu()
 
-l3_1 = Convolution(input_sizes=[batch_size, 5, 5, 64], filter_sizes=[3, 3, 64, 64], init='alexnet', strides=[1,1,1,1], padding="SAME", name='conv3')
-l3_2 = BatchNorm(input_size=[batch_size, 5, 5, 64], name='conv3_bn')
+l3_1 = Convolution(input_sizes=[batch_size, 5, 5, 64], filter_sizes=[3, 3, 64, 64], init='alexnet', strides=[1,1,1,1], padding="SAME", name='conv3', load=weights_conv)
+l3_2 = BatchNorm(input_size=[batch_size, 5, 5, 64], name='conv3_bn', load=weights_conv)
 l3_3 = Relu()
 
 l4 = ConvToFullyConnected(input_shape=[5, 5, 64])
-l5 = FullyConnected(input_shape=5*5*64, size=512, init='alexnet', activation=Relu(), bias=0., name='fc1')
-l6 = FullyConnected(input_shape=512, size=2, init='alexnet', activation=Linear(), bias=0., name='fc2')
+
+l5_1 = FullyConnected(input_shape=5*5*64, size=512, init='alexnet', name='fc1', load=weights_fc)
+l5_2 = BatchNorm(input_size=[batch_size, 512], name='fc1_bn', load=weights_fc)
+l5_3 = Relu()
+
+l6 = FullyConnected(input_shape=512, size=2, init='alexnet', name='fc2', load=weights_fc)
 
 model = Model(layers=[l1_1, l1_2, l1_3, l1_4, \
                       l2_1, l2_2, l2_3,       \
                       l3_1, l3_2, l3_3,       \
                       l4,                     \
-                      l5,                     \
+                      l5_1, l5_2, l5_3,       \
                       l6,                     \
                       ])
 

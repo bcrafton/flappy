cdee4b7fd81659d8b61f0cfd1e53b085b0a0c2e2
Brian Crafton <crafton.b@husky.neu.edu>
2019-05-25 20:25:01 -0400

    using old framework, gym's flappy bird wrapper

--- flappy_bird.py
+++ flappy_bird.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--lr', type=float, default=1e-4)
+parser.add_argument('--lr', type=float, default=2.5e-4)
 parser.add_argument('--eps', type=float, default=1.)
 parser.add_argument('--gpu', type=int, default=0)
 parser.add_argument('--name', type=str, default="flappy")
@@ -38,7 +38,7 @@ from lib.Activation import Relu
 from lib.Activation import Linear
 
 batch_size = 64
-total_steps = int(1e6)
+total_steps = int(1e7)
 epsilon_init = 0.1
 decay_rate = epsilon_init / (1.0 * total_steps)
 
@@ -51,7 +51,7 @@ class FlappyBirdEnv:
         self.total_reward = 0.0
         self.total_step = 0
         self.state = None
-        self.frame_skip = 2
+        self.frame_skip = 1
 
     def reset(self):
         self.total_reward = 0.0
@@ -69,6 +69,7 @@ class FlappyBirdEnv:
         self.total_step += 1
         self.total_reward += reward
 
+        '''
         for _ in range(self.frame_skip):
             if done:
                 break
@@ -77,6 +78,7 @@ class FlappyBirdEnv:
             reward = self._reward_shaping(reward)
             self.total_step += 1
             self.total_reward += reward
+        '''
         
         next_frame = self._process(next_frame)
         self.state.append(next_frame)
@@ -91,13 +93,19 @@ class FlappyBirdEnv:
         else:
             return 0.01
 
-    def _process(self, state):
+    def _process(self, frame):
+        '''
         output = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
         output = output[:410, :]
         output = cv2.resize(output, (80, 80))
         output = output / 255.0
         return output
-
+        '''
+        x_t = cv2.cvtColor(cv2.resize(frame, (80, 80)), cv2.COLOR_BGR2GRAY)
+        ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
+        return x_t
+        
+        
 ####################################
 
 train_fc = True
@@ -112,33 +120,33 @@ s = tf.placeholder("float", [None, 80, 80, 4])
 a = tf.placeholder("float", [None, 2])
 y = tf.placeholder("float", [None])
 
-l1_1 = Convolution(input_sizes=[batch_size, 80, 80, 4], filter_sizes=[8, 8, 4, 32], init='alexnet', strides=[1,4,4,1], padding="SAME", name='conv1', load=weights_conv)
-l1_2 = BatchNorm(input_size=[batch_size, 20, 20, 32], name='conv1_bn', load=weights_conv)
+l1_1 = Convolution(input_sizes=[batch_size, 80, 80, 4], filter_sizes=[8, 8, 4, 32], init='sqrt_fan_in', strides=[1,4,4,1], padding="SAME", name='conv1', load=weights_conv)
+# l1_2 = BatchNorm(input_size=[batch_size, 20, 20, 32], name='conv1_bn', load=weights_conv)
 l1_3 = Relu()
 l1_4 = MaxPool(size=[batch_size, 20, 20, 32], ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-l2_1 = Convolution(input_sizes=[batch_size, 10, 10, 32], filter_sizes=[4, 4, 32, 64], init='alexnet', strides=[1,2,2,1], padding="SAME", name='conv2', load=weights_conv)
-l2_2 = BatchNorm(input_size=[batch_size, 5, 5, 64], name='conv2_bn', load=weights_conv)
+l2_1 = Convolution(input_sizes=[batch_size, 10, 10, 32], filter_sizes=[4, 4, 32, 64], init='sqrt_fan_in', strides=[1,2,2,1], padding="SAME", name='conv2', load=weights_conv)
+# l2_2 = BatchNorm(input_size=[batch_size, 5, 5, 64], name='conv2_bn', load=weights_conv)
 l2_3 = Relu()
 
-l3_1 = Convolution(input_sizes=[batch_size, 5, 5, 64], filter_sizes=[3, 3, 64, 64], init='alexnet', strides=[1,1,1,1], padding="SAME", name='conv3', load=weights_conv)
-l3_2 = BatchNorm(input_size=[batch_size, 5, 5, 64], name='conv3_bn', load=weights_conv)
+l3_1 = Convolution(input_sizes=[batch_size, 5, 5, 64], filter_sizes=[3, 3, 64, 64], init='sqrt_fan_in', strides=[1,1,1,1], padding="SAME", name='conv3', load=weights_conv)
+# l3_2 = BatchNorm(input_size=[batch_size, 5, 5, 64], name='conv3_bn', load=weights_conv)
 l3_3 = Relu()
 
 l4 = ConvToFullyConnected(input_shape=[5, 5, 64])
 
-l5_1 = FullyConnected(input_shape=5*5*64, size=512, init='alexnet', name='fc1', load=weights_fc)
-l5_2 = BatchNorm(input_size=[batch_size, 512], name='fc1_bn', load=weights_fc)
+l5_1 = FullyConnected(input_shape=5*5*64, size=512, init='sqrt_fan_in', name='fc1', load=weights_fc)
+# l5_2 = BatchNorm(input_size=[batch_size, 512], name='fc1_bn', load=weights_fc)
 l5_3 = Relu()
 
-l6 = FullyConnected(input_shape=512, size=2, init='alexnet', name='fc2', load=weights_fc)
+l6 = FullyConnected(input_shape=512, size=2, init='sqrt_fan_in', name='fc2', load=weights_fc)
 
-model = Model(layers=[l1_1, l1_2, l1_3, l1_4, \
-                      l2_1, l2_2, l2_3,       \
-                      l3_1, l3_2, l3_3,       \
-                      l4,                     \
-                      l5_1, l5_2, l5_3,       \
-                      l6,                     \
+model = Model(layers=[l1_1, l1_3, l1_4, \
+                      l2_1, l2_3,       \
+                      l3_1, l3_3,       \
+                      l4,               \
+                      l5_1, l5_3,       \
+                      l6,               \
                       ])
 
 ####################################
@@ -196,8 +204,9 @@ for e in range(total_steps):
     '''
     
     if done:
-        p = "%d %f" % (e, env.total_reward)
+        p = "%d %f %d" % (e, env.total_reward, len(action_list))
         print (p)
+        print (action_list)
         f = open(filename, "a")
         f.write(p + "\n")
         f.close()
@@ -207,7 +216,7 @@ for e in range(total_steps):
     
     #####################################
 
-    if e > 1000:
+    if e > 100:
         # could be far more efficient here if we stored the processed state, action, y values somewhere else.
         minibatch = random.sample(replay_buffer, batch_size)
 

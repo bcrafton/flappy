f770010187ec8590c6a79caf313fa92fb3009abe
Brian Crafton <crafton.b@husky.neu.edu>
2019-06-13 11:57:28 -0400

    moving the stuff around

--- lib/LELPPO.py
+++ lib/LELPPO.py
@@ -18,6 +18,35 @@ from lib.AvgPool import AvgPool
 from lib.Activation import Relu
 from lib.Activation import Linear
 
+# so we actually dont need ppo_cache in :
+# def lel_backward(self, AI, AO, DO, ppo_cache):
+# bc we shud be just get it when sess.run is called. 
+# i think its better to call sess.run once ...
+# cud do either way tho...
+
+# where every LELPPO layer pulls the accumulated things it said to cache in predict. 
+# and calls sess.run to create a new DO.
+# but we wud already be running a session so nah ...
+
+####################################################
+
+def sample(logits):
+    uniform = tf.random_uniform(tf.shape(logits))
+    return tf.argmax(logits - tf.log(-tf.log(uniform)), axis=-1)
+
+def policy_entropy(logits):
+    a = logits - tf.reduce_max(logits, axis=-1, keepdims=True)
+    exp_a = tf.exp(a)
+    z = tf.reduce_sum(exp_a, axis=-1, keepdims=True)
+    p = exp_a / z
+    return tf.reduce_sum(p * (tf.log(z) - a), axis=-1)
+
+def neg_log_prob(logits, actions):
+    one_hot_actions = tf.one_hot(actions, action_dim)
+    return -tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=one_hot_actions, dim=-1)
+    
+####################################################
+
 class LELPPO(Layer):
 
     def __init__(self, input_shape, pool_shape, nactions, name=None):
@@ -30,14 +59,14 @@ class LELPPO(Layer):
         self.value_name = self.name + '_value'
         self.nlp_name = self.name + '_nlp'
         
-        l1 = AvgPool(size=self.input_shape, ksize=self.pool_shape, strides=self.pool_shape, padding='SAME')
+        self.pool = AvgPool(size=self.input_shape, ksize=self.pool_shape, strides=self.pool_shape, padding='SAME')
 
         l2_input_shape = l1.output_shape()
-        l2 = ConvToFullyConnected(input_shape=l2_input_shape)
+        self.conv2fc = ConvToFullyConnected(input_shape=l2_input_shape)
         
         l3_input_shape = l2.output_shape()
-        actions = FullyConnected(input_shape=l3_input_shape, size=self.nactions, init='alexnet', name=self.name + '_actions')
-        values = FullyConnected(input_shape=l3_input_shape, size=1, init='alexnet', name=self.name + '_values')
+        self.actions = FullyConnected(input_shape=l3_input_shape, size=self.nactions, init='alexnet', name=self.name + '_actions')
+        self.values = FullyConnected(input_shape=l3_input_shape, size=1, init='alexnet', name=self.name + '_values')
 
         ####################################################
 
@@ -49,8 +78,6 @@ class LELPPO(Layer):
 
         ####################################################
 
-        # we cant have a placeholder for this.
-        self.states = tf.placeholder("float", [None, 84, 84, 4])
         self.advantages = tf.placeholder("float", [None])
         self.rewards = tf.placeholder("float", [None]) 
         
@@ -60,30 +87,6 @@ class LELPPO(Layer):
         
         ####################################################
         
-        [self.logits, self.logits_forward] = self.actions_model.forward(self.states)
-        [self.values, self.values_forward] = self.values_model.forward(self.states)
-        
-        self.logits_train = self.logits + self.logits_bias
-        self.values_train = self.values + self.values_bias
-
-        self.values       = tf.reshape(self.values,       (-1,))
-        self.values_train = tf.reshape(self.values_train, (-1,))
-        
-        ####################################################
-
-        # gonna have to get rid of pi i think ... 
-        # go to the old sample, neg_log_prob thing. 
-        # need to make sure equivalent in a test script. 
-        self.pi1 = tf.distributions.Categorical(logits=self.logits)
-        self.pi2 = tf.distributions.Categorical(logits=self.logits_train)
-
-        self.actions        = tf.squeeze(self.pi1.sample(1), axis=0)
-        
-        self.nlps1          = self.pi1.log_prob(self.actions)
-        self.nlps2          = self.pi2.log_prob(self.old_actions)
-
-    ###################################################################
-    
     def get_weights(self):
         return []
         
@@ -97,10 +100,15 @@ class LELPPO(Layer):
         return X
         
     def predict(self, X):
-        # we are gonna need to move stuff around the constructor. 
+        [logits, logits_forward] = self.actions_model.forward(X) 
+        [values, values_forward] = self.values_model.forward(X)
         
+        values = tf.reshape(values, (-1,))
+        actions = sample(logits)
+        nlps = neg_log_prob(logits, actions) 
+
         # states, rewards, advantages, old_actions, old_values, old_nlps
-        cache = {self.action_name: self.actions, self.value_name: self.values, self.nlp_name: self.nlps1}
+        cache = {self.action_name: actions, self.value_name: values, self.nlp_name: nlps}
         return X, cache
                 
     ###################################################################           
@@ -111,9 +119,6 @@ class LELPPO(Layer):
     def gv(self, AI, AO, DO):    
         return []
         
-    def train(self, AI, AO, DO): 
-        return []
-        
     ###################################################################
 
     def dfa_backward(self, AI, AO, E, DO):
@@ -122,21 +127,41 @@ class LELPPO(Layer):
     def dfa_gv(self, AI, AO, E, DO):
         return []
         
-    def dfa(self, AI, AO, E, DO): 
-        return []
-        
     ###################################################################   
         
-    def lel_backward(self, AI, AO, E, DO, Y):
-        # write this
+    def lel_backward(self, AI, AO, DO, cache):
+        [logits, logits_forward] = self.actions_model.forward(AI)
+        [values, values_forward] = self.values_model.forward(AI)
+        logits = logits + self.logits_bias
+        values = values + self.values_bias
+        values = tf.reshape(values, (-1,))
+        nlps = neg_log_prob(logits, self.old_actions)
         
-        
-    def lel_gv(self, AI, AO, E, DO, Y):
-        # write this
-        assert(False)
+        ratio = tf.exp(nlps - self.old_nlps)
+        ratio = tf.clip_by_value(ratio, 0, 10)
+        surr1 = self.advantages * ratio
+        surr2 = self.advantages * tf.clip_by_value(ratio, 1 - epsilon_decay, 1 + epsilon_decay)
+        policy_loss = -tf.reduce_mean(tf.minimum(surr1, surr2))
 
-    def lel(self, AI, AO, E, DO, Y): 
-        return []
+        entropy_loss = -policy_entropy(train)
+
+        clipped_value_estimate = self.old_values + tf.clip_by_value(values - self.old_values, -epsilon_decay, epsilon_decay)
+        value_loss_1 = tf.squared_difference(clipped_value_estimate, self.rewards)
+        value_loss_2 = tf.squared_difference(values, self.rewards)
+        value_loss = 0.5 * tf.reduce_mean(tf.maximum(value_loss_1, value_loss_2))
+
+        loss = policy_loss + 0.01 * entropy_loss + 1. * value_loss
+        # grads = tf.gradients(self.loss, [self.logits_bias, self.values_bias] + self.params)
+        grads = tf.gradients(self.loss, [self.logits_bias, self.values_bias])
+        
+        do_logits = grads[0]
+        do_values = grads[1]
+        
+        dlogits = 
+        
+    def lel_gv(self, AI, AO, DO, cache):
+        # make a cache in lel_backward() above and use it here.
+        assert(False)
         
     ###################################################################
         

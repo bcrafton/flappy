57415342dabd9104ed030eb9d354b5ddac5364be
Brian Crafton <crafton.b@husky.neu.edu>
2019-06-22 23:46:02 -0400

    layerwise training of ppo

--- lib/PPOModel.py
+++ lib/PPOModel.py
@@ -40,15 +40,27 @@ class PPOModel:
         self.old_nlps = tf.placeholder("float", [None])
 
         ##############################################
+        with tf.variable_scope('l1'):
+            conv1 = tf.layers.conv2d(self.states, 32, 8, 4, activation=tf.nn.relu)
+            flat1 = tf.layers.flatten(conv1)
+            self.value1 = tf.squeeze(tf.layers.dense(flat1, 1), axis=-1)
+        
+        with tf.variable_scope('l2'):
+            conv2 = tf.layers.conv2d(conv1, 64, 4, 2, activation=tf.nn.relu)
+            flat2 = tf.layers.flatten(conv2)
+            self.value2 = tf.squeeze(tf.layers.dense(flat2, 1), axis=-1)
 
-        conv1 = tf.layers.conv2d(self.states, 32, 8, 4, activation=tf.nn.relu)
-        conv2 = tf.layers.conv2d(conv1, 64, 4, 2, activation=tf.nn.relu)
-        conv3 = tf.layers.conv2d(conv2, 64, 3, 1, activation=tf.nn.relu)
-        flattened = tf.layers.flatten(conv3)
-        fc = tf.layers.dense(flattened, 512, activation=tf.nn.relu)
+        with tf.variable_scope('l3'):
+            conv3 = tf.layers.conv2d(conv2, 64, 3, 1, activation=tf.nn.relu)
+            flat3 = tf.layers.flatten(conv3)
+            self.value3 = tf.squeeze(tf.layers.dense(flat3, 1), axis=-1)
 
-        self.values = tf.squeeze(tf.layers.dense(fc, 1), axis=-1)
-        self.action_logits = tf.layers.dense(fc, 4)
+        with tf.variable_scope('l4'):
+            flattened = tf.layers.flatten(conv3)
+            fc = tf.layers.dense(flattened, 512, activation=tf.nn.relu)
+            self.values = tf.squeeze(tf.layers.dense(fc, 1), axis=-1)
+            self.action_logits = tf.layers.dense(fc, 4)
+
         self.action_dists = tf.distributions.Categorical(logits=self.action_logits)
         self.pi = self.action_dists
         
@@ -79,12 +91,36 @@ class PPOModel:
         value_loss_2 = tf.squared_difference(self.values, self.rewards)
         value_loss = 0.5 * tf.reduce_mean(tf.maximum(value_loss_1, value_loss_2))
 
+        ##############################################
+
+        clipped_value_estimate = self.old_values + tf.clip_by_value(self.value1 - self.old_values, -epsilon_decay, epsilon_decay)
+        value_loss_1 = tf.squared_difference(clipped_value_estimate, self.rewards)
+        value_loss_2 = tf.squared_difference(self.value1, self.rewards)
+        value_loss1 = 0.5 * tf.reduce_mean(tf.maximum(value_loss_1, value_loss_2))                
+
+        clipped_value_estimate = self.old_values + tf.clip_by_value(self.value2 - self.old_values, -epsilon_decay, epsilon_decay)
+        value_loss_1 = tf.squared_difference(clipped_value_estimate, self.rewards)
+        value_loss_2 = tf.squared_difference(self.value2, self.rewards)
+        value_loss2 = 0.5 * tf.reduce_mean(tf.maximum(value_loss_1, value_loss_2))
+        
+        clipped_value_estimate = self.old_values + tf.clip_by_value(self.value3 - self.old_values, -epsilon_decay, epsilon_decay)
+        value_loss_1 = tf.squared_difference(clipped_value_estimate, self.rewards)
+        value_loss_2 = tf.squared_difference(self.value3, self.rewards)
+        value_loss3 = 0.5 * tf.reduce_mean(tf.maximum(value_loss_1, value_loss_2))
+        
+        ##############################################
+
         self.loss = policy_loss + 0.01 * entropy_loss + 1. * value_loss
+        self.loss1 = value_loss1
+        self.loss2 = value_loss2
+        self.loss3 = value_loss3
 
         ##############################################
 
-        self.opt = tf.train.AdamOptimizer(learning_rate=self.lr, epsilon=self.eps)
-        self.train_op = self.opt.minimize(self.loss)
+        self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr, epsilon=self.eps).minimize(self.loss)
+        self.train_op1 = tf.train.AdamOptimizer(learning_rate=self.lr, epsilon=self.eps).minimize(self.loss1)
+        self.train_op2 = tf.train.AdamOptimizer(learning_rate=self.lr, epsilon=self.eps).minimize(self.loss2)
+        self.train_op3 = tf.train.AdamOptimizer(learning_rate=self.lr, epsilon=self.eps).minimize(self.loss3)
 
         global_step = tf.train.get_or_create_global_step()
         self.global_step_op = global_step.assign_add(1)
@@ -107,12 +143,9 @@ class PPOModel:
         return action, value, nlp
 
     def train(self, states, rewards, advantages, old_actions, old_values, old_nlps):
-        self.train_op.run(feed_dict={self.states:states, 
-                                     self.rewards:rewards, 
-                                     self.advantages:advantages, 
-                                     self.old_actions:old_actions, 
-                                     self.old_values:old_values, 
-                                     self.old_nlps:old_nlps})
+        # self.train_op.run(feed_dict={self.states:states, self.rewards:rewards, self.advantages:advantages, self.old_actions:old_actions, self.old_values:old_values, self.old_nlps:old_nlps})
+        # self.train_op.run(feed_dict={self.states:states, self.rewards:rewards, self.advantages:advantages, self.old_actions:old_actions, self.old_values:old_values, self.old_nlps:old_nlps})
+        self.sess.run([self.train_op, self.train_op1, self.train_op2, self.train_op3], feed_dict={self.states:states, self.rewards:rewards, self.advantages:advantages, self.old_actions:old_actions, self.old_values:old_values, self.old_nlps:old_nlps})
 
     ####################################################################
         
